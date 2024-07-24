e4800e6b3b367ffb861256f641327668eec205e0
PhuocHH2 <phuochh2@vng.com.vn>
2024-07-24 14:47:09 +0700

    update sample

---
+++ examples/simple-sample/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherApp from './WeatherApp';
+import './WeatherApp.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <WeatherApp />
+        </div>
+    );
+}
+
+export default App;
